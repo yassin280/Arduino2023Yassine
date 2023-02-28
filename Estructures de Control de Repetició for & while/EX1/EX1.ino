ca3a52bf1256ade664a8b66f68450135cdacd2d2
yassin280 <125276942+yassin280@users.noreply.github.com>
2023-02-28 13:25:40 +0100

    SD

---
+++ Estructures de Control de Repetició for & while/EX1/EX1.ino
@@ -0,0 +1,15 @@
+int comptar = 11;
+void setup()                // run once, when the sketch starts
+{
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.print("Ara comptare de 0 a ");
+  Serial.println(comptar);
+  for (int i=0; i <= comptar; i++)
+  {
+    Serial.print(i);
+    Serial.print("-");
+  } 
+}
+void loop()     // we need this to be here even though its empty
+{
+}
