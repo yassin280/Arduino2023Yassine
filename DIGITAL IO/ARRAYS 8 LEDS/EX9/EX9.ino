a0d1d83b45a981ef8e8b9af2da30ccf199baf98e
yassin280 <125276942+yassin280@users.noreply.github.com>
2023-03-07 11:41:48 +0100

    XNXX

---
+++ DIGITAL IO/ARRAYS 8 LEDS/EX9/EX9.ino
@@ -0,0 +1,52 @@
+/**********************************************************************************
+**                                                                               **
+**                              Array de 8 LEDs                                  **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const byte ledPin[] = {5, 6, 7, 8};   // donar nom en un array als pins 5, 6, 7 i 8
+const byte buttonPin = 2;             // donar nom al pin 2 de l’Arduino
+boolean buttonEstat = LOW;           // definir variable d'estat pel polsador
+
+int ledNum = 4;                   // definir variable de número de leds
+int num = 0;                      // definir variable del número a mostrar
+
+//********** Setup ****************************************************************
+void setup()
+{
+  for(int i = 0; i < ledNum; i++) // definir els pins 5, 6, 7 i 8 com sortides
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+  pinMode(buttonPin, INPUT);      // definir el pin 2 com una entrada
+  for(int j = 0; j < ledNum ; j++)     // els leds mostren incialment 0
+    { 
+      digitalWrite(ledPin[j], 0);
+    }
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  buttonEstat = digitalRead(buttonPin);
+  if (buttonEstat == HIGH)
+  { 
+    num = num + 1;                 // incrementar el número a mostrar
+    if (num == 16)       // quan ja ha ensenyat el 15 tornar al 0  
+           {
+              num = 0;
+           }    
+for(int j = 0; j < ledNum ; j++)   // actualitzar estat leds per mostrar número
+    { 
+      digitalWrite(ledPin[j], bitRead(num, j));
+    }
+    delay(500);    // per evitar que en una pulsació curta salti més d'un número 
+  }
+}
+
+//********** Funcions *************************************************************
+
